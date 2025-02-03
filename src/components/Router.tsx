eaf237fdc695637db3c4122f1cab4afaacaf4976
Cardin <nekk.ng3@gmail.com>
2025-02-03 13:21:38 -0800

    Implemented configpage and homepage

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,13 +1,14 @@
 import { Router, Route } from 'preact-router';
-// import ConfigPage from './ConfigPage';
-// import HomePage from './HomePage';
+import HomePage from './HomePage';
+import ConfigPage from './ConfigPage';
 import { App } from '../app.tsx';
 
 const AppRouter = () => {
   return (
     <Router>
-      <Route path="/" component={App} />
-      {/*<Route path="" component={App} />*/}
+      {/*<Route path="/" component={App} />*/}
+      <Route path="/" component={HomePage} />
+      <Route path="/:year" component={ConfigPage} />
     </Router>
   );
 };
